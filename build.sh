fc89ca482504fb9f0fb4b7479538a50de3142be8
glorieux-f <frederic.glorieux@fictif.org>
2016-03-15 13:17:43 +0100

    essai de centrage des poèmes

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-BASEDIR=$(dirname $0)
-# pour les noms de fichiers avec accents
-export LC_CTYPE=fr_FR.UTF-8
-/usr/bin/svn --non-interactive --trust-server-cert  update $BASEDIR/ 2>&1
-/usr/bin/php $BASEDIR/../teipub/Teipub.php  "$BASEDIR/xml/*.xml"  "$BASEDIR/gongora.sqlite" 2>&1
-exit 0
